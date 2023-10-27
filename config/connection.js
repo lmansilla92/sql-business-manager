fe1a89d3e017b42c57c8f00f9adaf8431c3bec8e
Luis Mansilla <l_mansilla92@yahoo.com>
2023-10-27 14:58:03 -0500

    Created tables for department, role, and employee in the schemal.sql

--- config/connection.js
+++ config/connection.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
+    database='business_db',
     {
         host: '127.0.0.1',
         dialect: 'mysql',
