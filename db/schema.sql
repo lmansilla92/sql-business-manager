28d942bfcbe4f6b7a7e5a8cf88772e069ef6cf53
Luis Mansilla <l_mansilla92@yahoo.com>
2023-10-27 18:44:14 -0500

    Added NOT NULL and ON DELETE SET NULL in schema, added data to insert into tables in the seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,28 +2,41 @@ DROP DATABASE IF EXISTS business_db;
 CREATE DATABASE business_db;
 USE business_db;
 
-CREATE TABLE department(
+CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id)
-    REFERENCES department(id);
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES role(id),
+    REFERENCES role(id)
+    ON DELETE SET NULL,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
+    ON DELETE SET NULL
 );
+
+-- View all employees
+-- SELECT * FROM employee;
+-- Add employee
+-- Update employee role
+-- View all roles
+-- Add Role
+-- View all departments
+-- Add department
+-- Quit
